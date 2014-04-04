ea8e53f3fa4bdfe8d52e6632afab4cdfbbace37b
rieske <vaidotas.valuckas@gmail.com>
2014-04-04 18:57:18 +0300

    adding 8 threads for code analysis

--- reports.sh
+++ reports.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir build
-cppcheck -v --enable=all --xml --xml-version=1 -I/usr/include/c++/4.8.1/ src/ 2> build/cppcheck-report.xml
+cppcheck -v --enable=all --xml --xml-version=1 -j 8 -I/usr/include/c++/4.8.1/ src/ 2> build/cppcheck-report.xml
 
