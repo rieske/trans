f7992eb9395ad70f1e58a45be70d90c47269aafc
rieske <vaidotas.valuckas@gmail.com>
2014-04-05 08:44:06 +0300

    added valgrind report generation

--- reports.sh
+++ reports.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mkdir build
 cppcheck -v --enable=all --xml --xml-version=1 -j 8 -I/usr/include/c++/4.8.1/ src/ 2> build/cppcheck-report.xml
+valgrind --xml=yes --xml-file=build/valgrind-report.xml ./trans test_prog1
 
