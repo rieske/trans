25435bec5c8f07dec71e2e3f06e0a0fcc6d07710
rieske <vaidotas.valuckas@gmail.com>
2014-04-05 12:39:26 +0300

    adding valgrind reports

--- reports.sh
+++ reports.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-mkdir build
+mkdir -p build
 
 cppcheck -v --enable=all --xml --xml-version=1 -j 8 -Isrc src 2> build/cppcheck-report.xml
 sed -i.bak s/\ file=\"src\\//\ file=\".\\/src\\//g build/cppcheck-report.xml
 
-#valgrind --xml=yes --xml-file=build/valgrind-report.xml ./trans test_prog1
-
 find src -regex ".*\.cpp\|.*\.h" | vera++ - -showrules -nodup |& scripts/vera++Report2checkstyleReport.perl > build/vera-report.xml
 
 rats -w 3 --xml src > build/rats-report.xml
+
+mkdir -p output # FIXME
+valgrind --xml=yes --xml-file=build/valgrind-report.xml ./trans test_prog1
