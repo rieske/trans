63304d2b2327c4d70efca86dcaf4cc615367d8c1
rieske <vaidotas.valuckas@gmail.com>
2014-04-05 16:33:38 +0300

    adding gtest initial configuration

---
+++ test/dummy_test.cpp
@@ -0,0 +1,5 @@
+#include "gtest/gtest.h"
+
+TEST(DummyTest, Dummy) {
+    EXPECT_EQ(1, 1);
+}
---
+++ test/dummy_test2.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+
+TEST(DummyTest2, Dummy) {
+    EXPECT_EQ(1, 1);
+}
+
+TEST(DummyTest, Dummy2) {
+    EXPECT_EQ(2, 2);
+}
