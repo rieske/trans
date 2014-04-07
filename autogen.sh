f74277bdfc766000965e68847c84280414f30b6d
valuckav <valuckav@visa.com>
2014-04-07 19:37:54 +0100

    fixing build for ubuntu system

--- autogen.sh
+++ autogen.sh
@@ -2,5 +2,6 @@
  
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
+mkdir -p m4 config
 autoreconf --force --install --verbose -I config -I m4 "$srcdir"
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
