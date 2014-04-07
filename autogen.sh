c04b1e99f5d06fdde53a5b7d9ae13efc2e13c2b8
valuckav <valuckav@visa.com>
2014-04-07 19:30:31 +0100

    fixing autoconf for Red Hat system

--- autogen.sh
+++ autogen.sh
@@ -2,5 +2,5 @@
  
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
-autoreconf --force --install --verbose "$srcdir"
+autoreconf --force --install --verbose -I config -I m4 "$srcdir"
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
