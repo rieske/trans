dd311dfc0e240da6a9a27269371b32675a106359
rieske <vaidotas.valuckas@gmail.com>
2014-04-20 11:54:40 +0300

    adding test coverage reporting

--- reports.sh
+++ reports.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 mkdir -p build
 
+gcovr -x -r . > build/gcovr-report.xml
+
 cppcheck -v --enable=all --xml --xml-version=1 -j 8 -Isrc src 2> build/cppcheck-report.xml
 sed -i.bak s/\ file=\"src\\//\ file=\".\\/src\\//g build/cppcheck-report.xml
 
