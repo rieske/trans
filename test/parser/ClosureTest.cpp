e33d2374ac5478063aa362d310adb2e6dbc5a129
rieske <vaidotas.valuckas@gmail.com>
2015-01-22 20:27:17 +0200

    adding closure test

--- test/parser/ClosureTest.cpp
+++ test/parser/ClosureTest.cpp
@@ -1,2 +1,23 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "parser/BNFFileGrammar.h"
 #include "parser/Closure.h"
 
+namespace {
+
+using namespace parser;
+using namespace testing;
+
+TEST(Closure, computesClosure) {
+    BNFFileGrammar grammar { "test/grammars/closure_grammar.bnf" };
+    FirstTable firstTable { grammar.getNonterminals() };
+    Closure closure { firstTable };
+
+    std::vector<LR1Item> items { LR1Item { grammar.getStartSymbol(), 0, { grammar.getEndSymbol() } } };
+    closure(items);
+
+    EXPECT_THAT(items, SizeIs(6));
+}
+
+}
