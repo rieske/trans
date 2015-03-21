4b6b2d972c0dc49fbba525a1a0528a829b2a4645
rieske <vaidotas.valuckas@gmail.com>
2015-03-21 22:06:23 +0200

    preparing to hook in new code generator

--- src/driver/Compiler.cpp
+++ src/driver/Compiler.cpp
@@ -2,16 +2,22 @@
 
 #include <iostream>
 #include <vector>
+#include <memory>
 
+#include "../ast/AbstractSyntaxTree.h"
 #include "../code_generator/code_generator.h"
 #include "../code_generator/quadruple.h"
-#include "../semantic_analyzer/SymbolTable.h"
 #include "../parser/Parser.h"
+#include "../parser/SyntaxTree.h"
 #include "../parser/SyntaxTreeBuilder.h"
 #include "../scanner/Scanner.h"
-#include "../ast/AbstractSyntaxTree.h"
 #include "../semantic_analyzer/SemanticAnalyzer.h"
+#include "../semantic_analyzer/SymbolTable.h"
 #include "CompilerComponentsFactory.h"
+
+#include "code_generator/InstructionSet.h"
+#include "code_generator/StackMachine.h"
+#include "code_generator/AssemblyGenerator.h"
 
 using std::string;
 using std::unique_ptr;
@@ -23,7 +29,8 @@ using semantic_analyzer::SemanticAnalyzer;
 
 Compiler::Compiler(const CompilerComponentsFactory* compilerComponentsFactory) :
         compilerComponentsFactory { compilerComponentsFactory },
-        parser { this->compilerComponentsFactory->getParser() } {
+        parser { this->compilerComponentsFactory->getParser() }
+{
 }
 
 Compiler::~Compiler() {
@@ -44,6 +51,12 @@ void Compiler::compile(string sourceFileName) const {
 
     unique_ptr<SemanticAnalyzer> semanticAnalyzer { compilerComponentsFactory->newSemanticAnalyzer() };
     syntaxTree->analyzeWith(*semanticAnalyzer);
+
+    auto instructions = std::make_unique<code_generator::InstructionSet>();
+    std::ofstream outputFile { sourceFileName + ".S" };
+    auto stackMachine = std::make_unique<code_generator::StackMachine>(&outputFile, std::move(instructions));
+    code_generator::AssemblyGenerator assemblyGenerator { std::move(stackMachine) };
+    //assemblyGenerator.generateAssemblyCode(semanticAnalyzer->getQuadrupleCode());
 
     std::vector<code_generator::Quadruple_deprecated> quadrupleCode = semanticAnalyzer->getQuadrupleCode();
 
