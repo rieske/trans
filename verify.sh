75034fca76862e5ab3ad72f90fa15a5b50ecd597
Vaidotas Valuckas <vvaluckas@gradle.com>
2020-05-01 18:42:15 +0300

    Remove unused configuration

--- verify.sh
+++ verify.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-./gradlew check
+cmake .
+make
 ./trans fibb.src
 ./trans -gresources/configuration/grammar.bnf -lp fibb.src
 cp logs/parsing_table resources/configuration/parsing_table
