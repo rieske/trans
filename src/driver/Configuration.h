496cb26b0c731b19492e8b9579a7f6da77e689d1
Vaidotas Valuckas <vvaluckas@gradle.com>
2020-05-31 18:49:26 +0300

    Reformat file

--- src/driver/Configuration.h
+++ src/driver/Configuration.h
@@ -5,19 +5,19 @@
 #include <vector>
 
 class Configuration {
-public:
+  public:
     Configuration() = default;
-	Configuration(const Configuration& that) = delete;
+    Configuration(const Configuration &that) = delete;
 
-	virtual ~Configuration() = default;
+    virtual ~Configuration() = default;
 
-	virtual std::vector<std::string> getSourceFileNames() const = 0;
+    virtual std::vector<std::string> getSourceFileNames() const = 0;
     virtual std::string getLexFileName() const = 0;
-	virtual std::string getGrammarFileName() const = 0;
-	virtual std::string getParsingTableFileName() const = 0;
-	virtual bool usingCustomGrammar() const = 0;
-	virtual bool isParserLoggingEnabled() const = 0;
-	virtual bool isScannerLoggingEnabled() const = 0;
+    virtual std::string getGrammarFileName() const = 0;
+    virtual std::string getParsingTableFileName() const = 0;
+    virtual bool usingCustomGrammar() const = 0;
+    virtual bool isParserLoggingEnabled() const = 0;
+    virtual bool isScannerLoggingEnabled() const = 0;
 };
 
 #endif /* CONFIGURATION_H_ */
