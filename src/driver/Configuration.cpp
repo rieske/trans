f4d6c8b499455396ed164a016777f08883eb4df0
Vaidotas Valuckas <vvaluckas@gradle.com>
2021-02-06 20:30:17 +0200

    Add missed source file

---
+++ src/driver/Configuration.cpp
@@ -0,0 +1,66 @@
+#include "Configuration.h"
+
+void Configuration::setSourceFiles(std::vector<std::string> sourceFiles) {
+    this->sourceFiles = sourceFiles;
+}
+
+void Configuration::setResourcesBasePath(std::string resourcesBasePath) {
+    this->resourcesBasePath = resourcesBasePath;
+}
+
+void Configuration::setLexPath(std::string lexPath) {
+    this->lexPath = lexPath;
+}
+
+void Configuration::setGrammarPath(std::string grammarPath) {
+    this->grammarPath = grammarPath;
+    this->customGrammar = true;
+}
+
+void Configuration::setParsingTablePath(std::string parsingTablePath) {
+    this->parsingTablePath = parsingTablePath;
+}
+
+void Configuration::enableParserLogging() {
+    this->parserLogging = true;
+}
+
+void Configuration::enableScannerLogging() {
+    this->scannerLogging = true;
+}
+
+void Configuration::setOutputIntermediateForms() {
+    this->outputIntermediateForms = true;
+}
+
+std::vector<std::string> Configuration::getSourceFiles() const {
+    return sourceFiles;
+}
+
+std::string Configuration::getLexPath() const {
+    return resourcesBasePath + lexPath;
+}
+
+std::string Configuration::getGrammarPath() const {
+    return resourcesBasePath + grammarPath;
+}
+
+std::string Configuration::getParsingTablePath() const {
+    return resourcesBasePath + parsingTablePath;
+}
+
+bool Configuration::usingCustomGrammar() const {
+    return customGrammar;
+}
+
+bool Configuration::isParserLoggingEnabled() const {
+    return parserLogging;
+}
+
+bool Configuration::isScannerLoggingEnabled() const {
+    return scannerLogging;
+}
+
+bool Configuration::isOutputIntermediateForms() const {
+    return outputIntermediateForms;
+}
