755409718360b986e7a3ef2a699eda40f045a850
Vaidotas Valuckas <vvaluckas@gradle.com>
2021-02-14 15:26:29 +0200

    Expand while loop test coverage

--- test/src/functionalTest/WhileLoopTest.cpp
+++ test/src/functionalTest/WhileLoopTest.cpp
@@ -24,5 +24,33 @@ TEST(Compiler, compilesWhileLoopSumProgram) {
     program.runAndExpect("10\n");
 }
 
+TEST(Compiler, whileIterationOutput) {
+    SourceProgram program{R"prg(
+        int whileOutput(int it) {
+            while(it > 0) {
+                output it;
+                it = it - 1;
+            }
+            return 0;
+        }
+
+        int main() {
+            int it;
+            input it;
+            whileOutput(it);
+            return 0;
+        }
+    )prg"};
+
+    program.compile();
+
+    program.runAndExpect("1", "1\n");
+    program.runAndExpect("10", "10\n9\n8\n7\n6\n5\n4\n3\n2\n1\n");
+    program.runAndExpect("0", "");
+
+    // FIXME: gets into an endless loop
+    //program.runAndExpect("-1", "");
+}
+
 }
 
