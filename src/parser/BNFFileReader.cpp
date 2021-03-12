8069a0c1e20b8c31c241556ddcc1d619dcbdb1e9
Vaidotas Valuckas <vvaluckas@gradle.com>
2021-03-12 17:28:56 +0200

    Rename BNFFileReader

---
+++ src/parser/BNFFileReader.cpp
@@ -0,0 +1,75 @@
+#include "BNFFileReader.h"
+#include "parser/GrammarBuilder.h"
+
+#include <fstream>
+#include <stdexcept>
+#include <algorithm>
+#include <map>
+
+#include <iostream>
+
+namespace {
+
+const std::string TERMINAL_CONFIG_DELIMITER = "%%";
+const char NONTERMINAL_START = '<';
+const char NONTERMINAL_END = '>';
+const char TERMINAL_START = '\'';
+const char TERMINAL_END = '\'';
+
+} // namespace
+
+namespace parser {
+
+Grammar BNFFileReader::readGrammar(const std::string bnfFileName) const {
+    std::ifstream bnfInputStream { bnfFileName };
+    if (!bnfInputStream.is_open()) {
+        throw std::invalid_argument("Unable to open bnf file for reading: " + bnfFileName);
+    }
+
+    std::vector<std::string> symbolsBeingDefined;
+    std::string nonterminalName;
+    std::vector<std::string> producedSymbolNames;
+
+    GrammarBuilder builder;
+
+    for (std::string bnfToken; bnfInputStream >> bnfToken && bnfToken != TERMINAL_CONFIG_DELIMITER;) {
+        if (bnfToken.length() == 1) {
+            switch (bnfToken.front()) {
+                case '|': {
+                    builder.defineRule(nonterminalName, producedSymbolNames);
+                    producedSymbolNames.clear();
+                    break;
+                }
+                case ';': {
+                    builder.defineRule(nonterminalName, producedSymbolNames);
+                    producedSymbolNames.clear();
+                    nonterminalName.clear();
+                    break;
+                }
+                case ':':
+                    break;
+                default:
+                    throw std::runtime_error("Unrecognized control character in grammar configuration file: " + bnfToken);
+            }
+        } else if (!bnfToken.empty() && bnfToken.front() == NONTERMINAL_START && bnfToken.back() == NONTERMINAL_END) {
+            const auto& nonterminalBeingDefinedIterator = std::find(symbolsBeingDefined.begin(), symbolsBeingDefined.end(), bnfToken);
+            if (nonterminalBeingDefinedIterator == symbolsBeingDefined.end()) {
+                symbolsBeingDefined.push_back(bnfToken);
+            }
+            if (!nonterminalName.empty()) {
+                producedSymbolNames.push_back(bnfToken);
+            } else {
+                nonterminalName = bnfToken;
+            }
+        } else if (!bnfToken.empty() && bnfToken.front() == TERMINAL_START && bnfToken.back() == TERMINAL_END) {
+            producedSymbolNames.push_back(bnfToken.substr(1, bnfToken.size() - 2));
+        } else {
+            throw std::runtime_error("Unrecognized token in grammar configuration file: " + bnfToken);
+        }
+    }
+
+    return builder.build();
+}
+
+} // namespace parser
+
---
+++ src/parser/BNFFileReader.h
@@ -0,0 +1,19 @@
+#ifndef BNFREADER_H_
+#define BNFREADER_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "Grammar.h"
+
+namespace parser {
+
+class BNFFileReader {
+public:
+    Grammar readGrammar(const std::string bnfFileName) const;
+};
+
+} // namespace parser
+
+#endif // BNFREADER_H_
