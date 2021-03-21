34f6273779da1ce7d0ae8b9df794810564dc8b3a
Vaidotas Valuckas <vvaluckas@gradle.com>
2021-03-21 10:24:31 +0200

    Improve exception message

--- src/parser/Action.cpp
+++ src/parser/Action.cpp
@@ -22,27 +22,27 @@ std::unique_ptr<Action> Action::deserialize(std::string serializedAction, const 
     char type;
     actionStream >> type;
     switch (type) {
-    case SHIFT_ACTION: {
-        parse_state state;
-        actionStream >> state;
-        return std::make_unique<ShiftAction>(state);
-    }
-    case REDUCE_ACTION: {
-        size_t productionId;
-        actionStream >> productionId;
-        const auto& production = grammar.getRuleById(productionId);
-        return std::make_unique<ReduceAction>(production, &parsingTable);
-    }
-    case ACCEPT_ACTION:
-        return std::make_unique<AcceptAction>();
-    case ERROR_ACTION: {
-        parse_state state;
-        int expected;
-        actionStream >> state >> expected;
-        return std::make_unique<ErrorAction>(state, expected, &grammar);
-    }
-    default:
-        throw std::runtime_error("Error in parsing actionStream configuration file: invalid action type: " + std::to_string(type));
+        case SHIFT_ACTION: {
+            parse_state state;
+            actionStream >> state;
+            return std::make_unique<ShiftAction>(state);
+        }
+        case REDUCE_ACTION: {
+            size_t productionId;
+            actionStream >> productionId;
+            const auto& production = grammar.getRuleById(productionId);
+            return std::make_unique<ReduceAction>(production, &parsingTable);
+        }
+        case ACCEPT_ACTION:
+            return std::make_unique<AcceptAction>();
+        case ERROR_ACTION: {
+            parse_state state;
+            int expected;
+            actionStream >> state >> expected;
+            return std::make_unique<ErrorAction>(state, expected, &grammar);
+        }
+        default:
+            throw std::runtime_error("Error reading serialized parsing table: invalid action type: " + std::to_string(type));
     }
 }
 
